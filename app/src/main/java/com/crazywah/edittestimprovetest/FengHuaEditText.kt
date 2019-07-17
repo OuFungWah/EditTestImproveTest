f50e4d571f10c5160d9c418d854cfaf253409528
oufenghua <oufenghua@bytedance.com>
2019-07-17 18:26:21 +0800

    添加部分函数的监听,且增加新玩法

--- app/src/main/java/com/crazywah/edittestimprovetest/FengHuaEditText.kt
+++ app/src/main/java/com/crazywah/edittestimprovetest/FengHuaEditText.kt
@@ -1,41 +1,82 @@
 package com.crazywah.edittestimprovetest
 
 import android.content.Context
-import android.text.Editable
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.TextWatcher
+import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.text.*
+import android.text.style.BackgroundColorSpan
+import android.text.style.DynamicDrawableSpan
 import android.text.style.ForegroundColorSpan
+import android.text.style.ImageSpan
 import android.util.AttributeSet
 import android.util.Log
 import android.view.KeyEvent
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputConnection
 import android.view.inputmethod.InputConnectionWrapper
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.text.set
+import kotlin.math.abs
+import kotlin.random.Random
 
 class FengHuaEditText : AppCompatEditText {
 
-    companion object{
-        private val TAG : String = "FengHuaEditText"
+    companion object {
+        private val TAG: String = "FengHuaEditText"
+        private val random = Random(System.currentTimeMillis())
+        private var count = 0
+
+        fun randomColor(): Int {
+            return Color.parseColor(
+                "#${abs(random.nextInt(25, 256)).toString(16)}" +
+                        abs(random.nextInt(25, 256)).toString(16) +
+                        abs(random.nextInt(25, 256)).toString(16)
+            )
+        }
     }
 
     constructor(context: Context) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    private var start = 0
+    private var end = 0
+
     init {
-        addTextChangedListener(object : TextWatcher{
-            override fun afterTextChanged(p0: Editable?) {
-                Log.d(TAG,"TextWatcher --- afterTextChanged() p0 = $p0")
+        addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) {
+                Log.d(TAG, "TextWatcher --- afterTextChanged() editable = $editable")
+                var foregroundSpan = ForegroundColorSpan(randomColor())
+                var backgroundSpan = BackgroundColorSpan(randomColor())
+                if (start <= end) {
+                    editableText.setSpan(foregroundSpan, start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                    editableText.setSpan(backgroundSpan, start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                }
+                var changeText = editableText.subSequence(start,end)
+                Log.d(TAG, "TextWatcher --- afterTextChanged() changeText = $changeText")
+                if("icon"==changeText.toString()){
+                    Log.d(TAG, "TextWatcher --- afterTextChanged() 开始转换")
+                    var imageSpan = ImageSpan(context,R.mipmap.ic_launcher_round,DynamicDrawableSpan.ALIGN_CENTER)
+                    editableText.setSpan(imageSpan,start,end,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
             }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                Log.d(TAG,"TextWatcher --- beforeTextChanged() p0 = $p0 ; p1 = $p1 ; p2 = $p2 ; p3 = $p3")
+            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d(
+                    TAG,
+                    "TextWatcher --- beforeTextChanged() charSequence = $charSequence ; start = $start ; count = $count ; after = $after"
+                )
+                this@FengHuaEditText.start = start
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                Log.d(TAG,"TextWatcher --- onTextChanged() p0 = $p0 ; p1 = $p1 ; p2 = $p2 ; p3 = $p3")
+            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d(
+                    TAG,
+                    "TextWatcher --- onTextChanged() charSequence = $charSequence ; start = $start ; before = $before ; count = $count"
+                )
+                end = count + start
             }
 
         })
@@ -50,11 +91,11 @@ class FengHuaEditText : AppCompatEditText {
     // This method is called when the selection has changed, in case any subclasses would like to know.
     override fun onSelectionChanged(selStart: Int, selEnd: Int) {
         val curSpan = text?.getSpans(selStart, selEnd, ForegroundColorSpan::class.java)
-        if (curSpan!=null)
-        for(i in 0 until curSpan.size){
-            Log.d(TAG,"EditText --- onSelectionChanged ${curSpan[i].toString()}")
-        }
-        Log.d(TAG,"$text && selstart = $selStart ; selend = $selEnd")
+        if (curSpan != null)
+            for (i in 0 until curSpan.size) {
+                Log.d(TAG, "EditText --- onSelectionChanged ${curSpan[i].toString()}")
+            }
+        Log.d(TAG, "EditText --- onSelectionChanged() $text && selstart = $selStart ; selend = $selEnd")
         super.onSelectionChanged(selStart, selEnd)
     }
 
@@ -63,13 +104,13 @@ class FengHuaEditText : AppCompatEditText {
     override fun onTextContextMenuItem(id: Int): Boolean {
         when (id) {
             android.R.id.paste -> {
-                Log.d(TAG,"EditText --- paste callback")
+                Log.d(TAG, "EditText --- paste callback")
             }
             android.R.id.cut -> {
-                Log.d(TAG,"EditText --- cut callback")
+                Log.d(TAG, "EditText --- cut callback")
             }
             android.R.id.copy -> {
-                Log.d(TAG,"EditText --- copy callback")
+                Log.d(TAG, "EditText --- copy callback")
             }
             else -> {
 
@@ -80,11 +121,14 @@ class FengHuaEditText : AppCompatEditText {
 
     override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter)
-        Log.d(TAG,"EditText --- onTextChanged: text = $text ; start = $start ; lengthBefore = $lengthBefore ; lengthAfter = $lengthAfter")
+        Log.d(
+            TAG,
+            "EditText --- onTextChanged: text = $text ; start = $start ; lengthBefore = $lengthBefore ; lengthAfter = $lengthAfter"
+        )
     }
 
     override fun onCreateInputConnection(outAttrs: EditorInfo?): InputConnection {
-        val connection = FenghuaInputConnectionWrapper(super.onCreateInputConnection(outAttrs),true)
+        val connection = FenghuaInputConnectionWrapper(super.onCreateInputConnection(outAttrs), true)
 //        connection.sendKeyEvent(KeyEvent(0, 0, 0, KeyEvent.KEYCODE_DEL, 0, 0, 0, 0, KeyEvent.KEYCODE_ENDCALL))
 //        Log.d(TAG," connection ")
         return connection
@@ -96,26 +140,31 @@ class FengHuaEditText : AppCompatEditText {
         InputConnectionWrapper(target, mutable) {
 
         override fun deleteSurroundingText(beforeLength: Int, afterLength: Int): Boolean {
-            Log.d(TAG,"FenghuaInputConnectionWrapper--- deleteSurroundingText beforeLength = $beforeLength ; afterLength = $afterLength")
+            Log.d(
+                TAG,
+                "FenghuaInputConnectionWrapper--- deleteSurroundingText beforeLength = $beforeLength ; afterLength = $afterLength"
+            )
             return super.deleteSurroundingText(1, 0)
         }
 
         override fun sendKeyEvent(event: KeyEvent?): Boolean {
-            Log.d(TAG,"FenghuaInputConnectionWrapper--- sendKeyEvent keycode = ${event?.keyCode}")
+            Log.d(TAG, "FenghuaInputConnectionWrapper--- sendKeyEvent keycode = ${event?.keyCode}")
             return super.sendKeyEvent(event)
         }
 
         // 从 IME 传来的文本内容以及添加文本内容后光标所在的位置
         override fun commitText(text: CharSequence?, newCursorPosition: Int): Boolean {
-            Log.d(TAG,"FenghuaInputConnectionWrapper--- commitText text = $text")
+            Log.d(TAG, "FenghuaInputConnectionWrapper--- commitText text = $text")
             return super.commitText(text, newCursorPosition)
         }
 
         override fun setComposingText(text: CharSequence?, newCursorPosition: Int): Boolean {
-            Log.d(TAG,"FenghuaInputConnectionWrapper--- setComposingText text = $text")
+            Log.d(TAG, "FenghuaInputConnectionWrapper--- setComposingText text = $text")
             return super.setComposingText(text, newCursorPosition)
         }
 
     }
+
+
 
 }
